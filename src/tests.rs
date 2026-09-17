75185ef826737860d37d8f1ccdcd6f94412dfdb2
Ifortd <vanyalitvinko@gmail.com>
2026-09-17 19:46:36 +0100

    vague iterator upd

--- src/tests.rs
+++ src/tests.rs
@@ -23,39 +23,42 @@ fn test_traversal() {
 
     while !veit.at_the_end() {
        // println!("veit item: {}, arra item: {}", veit.get_im(), numbers[ veit.pos.clone() as usize ]);
-        assert_eq!( * veit.get_im(), numbers[ veit.pos.clone() as usize ]  );
-        veit.next();
+        assert_eq!(* veit.get_ref(), numbers[ veit.pos.clone() as usize ]  );
+        veit.move_next();
     }
 
-    veit.next();
+    veit.move_next();
     while !veit.at_the_end() {
         // println!("veit item: {}, arra item: {}", veit.get_im(), numbers[ veit.pos.clone() as usize ]);
-        assert_eq!( * veit.get_im(), numbers[ (veit.pos.clone() ) as usize % numbers.len()]  );
-        veit.next();
+        assert_eq!(* veit.get_ref(), numbers[ (veit.pos.clone() ) as usize % numbers.len()]  );
+        veit.move_next();
     }
 
-    veit.prev();
+    veit.move_prev();
     while !veit.at_the_end() {
-        println!("veit item: {}, pos {}, arra item: {}", veit.get_im(), veit.pos, numbers[ (veit.pos.clone() ) as usize % numbers.len() ]);
-        assert_eq!( * veit.get_im(), numbers[ (veit.pos.clone() ) as usize % numbers.len()]  );
-        veit.prev();
+        println!("veit item: {}, pos {}, arra item: {}", veit.get_ref(), veit.pos, numbers[ (veit.pos.clone() ) as usize % numbers.len() ]);
+        assert_eq!(* veit.get_ref(), numbers[ (veit.pos.clone() ) as usize % numbers.len()]  );
+        veit.move_prev();
     }
 
-    veit.prev();
+    veit.move_prev();
 
     while !veit.at_the_end() {
-        println!("veit item: {}, pos {}, arra item: {}", veit.get_im(), veit.pos, numbers[ (veit.pos.clone() ) as usize % numbers.len() ]);
-        assert_eq!( * veit.get_im(), numbers[ (veit.pos.clone() ) as usize % numbers.len()]  );
-        veit.prev();
+        println!("veit item: {}, pos {}, arra item: {}", veit.get_ref(), veit.pos, numbers[ (veit.pos.clone() ) as usize % numbers.len() ]);
+        assert_eq!(* veit.get_ref(), numbers[ (veit.pos.clone() ) as usize % numbers.len()]  );
+        veit.move_prev();
     }
 
 }
 
 #[test]
 fn test_push() {
+    println!("push test");
     let numbers = vec![1,2,3,4,5,6,7];
 
     let mut vel: Velist<i64> = Velist::new();
+
+
 
     for number in  & numbers {
         vel.push_last(*number);
@@ -65,16 +68,66 @@ fn test_push() {
 
 
     while !veit.at_the_end() {
-        println!("veit item: {}, pos {}, arra item: {}", veit.get_im(), veit.pos, numbers[ (veit.pos.clone() ) as usize % numbers.len() ]);
-        veit.next();
+        println!("veit item: {}, pos {}, arra item: {}", veit.get_ref(), veit.pos, numbers[ (veit.pos.clone() ) as usize % numbers.len() ]);
+        veit.move_next();
     }
 
-    veit.
+    for i in &mut ( vel.get_iter()) {
+        print!(" el {},", i)
+    }
+    println!();
 
+    println!("after iter ");
+    let mut veit = vel.get_iter();
+
+    while !veit.at_the_end() {
+        println!("veit item: {}, pos {}, arra item: {}", veit.get_ref(), veit.pos, numbers[ (veit.pos.clone() ) as usize % numbers.len() ]);
+        assert_eq!(* veit.get_ref(), numbers[ (veit.pos.clone() ) as usize % numbers.len()]  );
+        veit.move_next();
+    }
+
+
+    struct NoClone {
+        val: i64
+    }
+    let mut vel2: Velist<NoClone> = Velist::new();
+    vel2.push_first( NoClone{val: 1} );
+    vel2.push_first( NoClone{val: 2} );
+    vel2.push_first( NoClone{val: 3} );
+
+    //for i in  &mut vel2.get_iter() {
+    //    print!(" el {},", i)
+    //}
     //for i in veit {
 //
-    //}
+    }
+
+#[test]
+fn test_ambigious_iter() {
+    let numbers = vec![1,2,3,4,5,6,7];
+
+    let mut vel: Velist<i64> = Velist::new();
 
 
 
+    for number in  & numbers {
+        vel.push_last(*number);
+    }
+    
+    let mut amb = vel.get_vague_iter();
+    let mut amb2 = vel.get_vague_iter();
+    
+    
+
+    println!("amb item {}", amb.get(&mut vel));
+    amb.move_forward( &mut vel );
+    println!("amb item {}", amb.get(&mut vel));
+    amb.move_forward( &mut vel );
+
+    amb2.move_forward( &mut vel )
 }
+
+
+
+
+
--- src/velist.rs
+++ src/velist.rs
@@ -1,4 +1,4 @@
-use std::io::SeekFrom;
+//use std::io::SeekFrom;
 
 pub struct Velist<T> {
     data: Vec<Node<T>>,
@@ -16,9 +16,10 @@ struct Node<T> {
     prev: usize,
     // use option ?
     // whether this
+    #[deprecated]
     active: bool,
     writes_count: u64,
-    content: T
+    content: Option<T>
 }
 
 /*impl<T> Clone for Node<T> {
@@ -32,10 +33,61 @@ struct Node<T> {
 }*/
 
 
-pub struct AmbigiousIter<T> {
+pub struct VelistAmbigiousIter<T> {
     index: usize,
     last_known_id: u64,
     parent: *const Velist<T>
+}
+
+impl<T> VelistAmbigiousIter<T> where T: Copy {
+    pub fn get(&mut self, velist: &mut Velist<T>) -> T {
+        if !self.is_valid(velist) {
+            panic!()
+        };
+        velist.at( self.index ).clone().unwrap()
+    }
+}
+
+impl<'a, T> VelistAmbigiousIter<T> {
+    pub fn get_mut(&mut self, velist: &'a mut Velist<T>) -> &'a mut T {
+        if !self.is_valid(velist) {
+            panic!()
+        };
+        velist.get_element_n_mut(self.index)
+    }
+}
+
+impl<T> VelistAmbigiousIter<T> {
+
+    fn is_valid(&self, velist: & Velist<T>) -> bool {
+        if ( self.parent != velist ) {
+            return false
+        }
+        if ( velist.data[ self.index].writes_count != self.last_known_id ) {
+            return false
+        }
+        if  velist.data[ self.index].content.is_none() {
+            return false
+        }
+        return true
+    }
+
+    pub fn move_forward(&mut self, velist: &mut Velist<T> ) {
+        //let index = iterator.index;
+        if !self.is_valid(velist) {
+            panic!()
+        };
+        self.index = velist.get_next(self.index)
+    }
+
+    /*fn access_by_vague_iterator(&mut self, iterator: AmbigiousIter<T> ) -> &mut T {
+        //if (self. )
+        let index = iterator.index;
+        if ( self.data[index].writes_count != iterator.last_known_id ) {
+            panic!()
+        };
+        return self.data[index].content.as_mut().unwrap()
+    }*/
 }
 
 pub struct VelistIter<'a, T> {
@@ -52,14 +104,13 @@ pub struct VelistIterIm<'a, T> {
 
 pub trait VelistIterable<T> {
 
-    fn next(&mut self);
-    fn prev(&mut self);
+    fn move_next(&mut self);
+    fn move_prev(&mut self);
   //  fn pos(& self) -> usize;
 
     fn at_the_end(&self) -> bool;
 
 }
-
 
 impl  <'a, T> VelistIterIm<'a, T>  {
     fn new(index: usize, velist: &'a mut Velist<T>) -> Self<> {
@@ -71,20 +122,18 @@ impl  <'a, T> VelistIterIm<'a, T>  {
     }
 
     fn get(&self) -> & T {
-        self.parent.get_element_n_im( self.index )
+        self.parent.get_element_n_ref( self.index )
     }
 }
 
-
-
 impl <T> VelistIterable<T> for VelistIterIm<'_, T> {
 
-    fn next(&mut self) {
+    fn move_next(&mut self) {
         self.index = self.parent.get_next( self.index );
         self.pos += 1;
     }
 
-    fn prev(&mut self) {
+    fn move_prev(&mut self) {
         self.index = self.parent.get_prev( self.index );
         self.pos -= 1;
     }
@@ -105,35 +154,53 @@ impl<'a, T> VelistIter<'a, T> {
         }
     }
 
-    pub fn get(&mut self) -> &mut T {
-        self.parent.get_element_n( self.index )
+    pub fn get_mut(&mut self) -> &mut T {
+        self.parent.get_element_n_mut( self.index )
     }
 
-    pub fn get_im(&self) -> & T {
-        self.parent.get_element_n_im( self.index )
+    pub fn get_ref(&self) -> & T {
+        self.parent.get_element_n_ref( self.index )
     }
 
-    pub fn pop(self) {
-        self.parent.pop_me(self.index)
+    pub fn pop(&mut self) -> T {
+
+        let out = self.parent.pop_me(self.index);
+        self.move_next();
+        self.pos -= 1; // because .next adds one
+        out
     }
 }
 
 
-/*impl<'a, T> Iterator for VelistIter<'a, T> {
-    type Item = &'a mut T ;
+impl<'a, T> Iterator for VelistIter<'a, T> where T: Copy {
+    type Item = T ;
 
     fn next(&mut self) -> Option<Self::Item> {
-        Option::Some( self.get() )
+        //let out = Option::Some(self.parent.get_element_n_mut( self.index ));
+        //self.pos += 1;
+        let out =  self.parent.at( self.index );
+
+        self.move_next();
+        if self.index == self.parent.first {
+            return Option::None
+        }
+        out
+       // self.index = self.parent.get_next( self.index );
+        //if ( self.index == self.parent.first ) {
+      //      return Option::None
+       // }
+       // out
+        //Option::Some( self.get() )
     }
-}*/
+}
 
 impl <T> VelistIterable<T> for VelistIter<'_, T> {
 
-    fn next(&mut self) {
+    fn move_next(&mut self) {
         self.pos += 1;
         self.index = self.parent.get_next( self.index );
     }
-    fn prev(&mut self) {
+    fn move_prev(&mut self) {
         self.index = self.parent.get_prev( self.index );
         self.pos -= 1;
     }
@@ -148,13 +215,39 @@ impl<T> Node<T> {
         Node {
             next,
             prev,
-            content: content,  //&mut content as *mut T
+            content: Option::Some(content),  //&mut content as *mut T
             active: true,
             writes_count: 0
         }
     }
 }
 
+
+/*impl<T> Iterator for Velist<T> where T: Copy {
+    type Item = T ;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        //let out = Option::Some(self.parent.get_element_n_mut( self.index ));
+        //self.pos += 1;
+
+        let out = self.at(self.first);
+        self.first = self.get_next(self.first);
+        out
+
+        /*if self.is_empty() {
+            Option::None
+        } else {
+            Option::Some(self.pop_me(self.first))
+        }*/
+    }
+}*/
+
+
+impl<T> Velist<T> where T: Copy {
+    fn at(&mut self, n: usize) -> Option<T>{
+        self.data[n].content.clone()
+    }
+}
 
 impl<T> Velist<T> {
 
@@ -175,13 +268,13 @@ impl<T> Velist<T> {
         self.data[ self.first ].prev
     }
 
-    /*
-    Pushes obj to a Velist
-    and return index of where it was pushed
-    Unsafe because the only way to access obj after this method is to use returned index - and if you lose this index obj become unaccessible
-     */
+    ///
+    /// Pushes obj to a Velist and returns index of obj node in velist
+    /// # Unsafe
+    /// As if right after calling there is no element in velist pointing to index of obj, meaning if we lose return index - obj will stay in velist forever
+    /// resulting in memory leak
     unsafe fn push_ambigious(&mut self, obj: T) -> usize {
-        let obj_node = Node::new( obj, self.last(), self.first  );
+        let obj_node = Node::new( obj, Self::last(self), self.first  );
         self.size += 1;
 
         let next = if self.next_free.len() == 0 {
@@ -208,18 +301,19 @@ impl<T> Velist<T> {
         let new = unsafe{ self.push_ambigious(obj) };
 
         //upodating index of last item
-        let last_ind = self.last();
+        let last_ind = Self::last(self);
         self.data[ last_ind ].next = new;
         //  updating index of the first so it points to obj behin
         self.data[ self.first ].prev = new;
     }
 
+    /// Pushes element at the beginning
     pub fn push_first(&mut self, obj: T) {
 
         let new =  unsafe{ self.push_ambigious(obj) };
 
         //upodating index of last item
-        let last_ind = self.last();
+        let last_ind = Self::last(self);
         self.data[ last_ind ].next = new;
         // updating index of the first so it points to obj behind
         self.data[ self.first ].prev = new;
@@ -229,37 +323,53 @@ impl<T> Velist<T> {
         self.first = new;
     }
 
+    /// Returns true if there is no elements in this velist
     pub fn is_empty(&self) -> bool {
         if self.size == 0 {true} else {false}
     }
 
+    #[deprecated]
     pub fn get_first(&self) -> &Node<T> {
         if self.is_empty() {panic!()};
 
         &self.data[ self.first ]
     }
 
-    // returns reference to content
-    fn get_element_n(&mut self, n: usize) -> &mut T {
-        &mut self.data[n].content
-    }
-
-    // returns index of next element
+    /// Returns index of element after element n
     fn get_next(&self, cur_index: usize) -> usize {
         self.data[cur_index].next
     }
 
-    fn get_prev(&self, cur_index: usize) -> usize {
-        self.data[cur_index].prev
+    /// Returns index of element before element n
+    fn get_prev(&self, n: usize) -> usize {
+        self.data[n].prev
     }
 
-    // returns reference to content
-    fn get_element_n_im(& self, n: usize) -> & T {
-        & self.data[n].content
+    /// Pops element n
+    fn pop_element_n(&mut self, n: usize) -> T {
+        self.data[n].writes_count += 1;
+        self.data[n].active = false;
+        self.data[n].content.take().unwrap()
+    }
+
+    ///
+    /// Returns mutable to element n
+    /// # Panics
+    /// If element n has content = None
+    fn get_element_n_mut(&mut self, n: usize) -> &mut T {
+        self.data[n].content.as_mut().unwrap()
+    }
+
+    ///
+    /// Returns reference to element n
+    /// # Panics
+    /// If element n has content = None
+    fn get_element_n_ref(& self, n: usize) -> & T {
+         self.data[n].content.as_ref().unwrap()
     }
 
     //unsafe? UNDONE
-    fn pop_me(&mut self, index: usize) {
+    fn pop_me(&mut self, index: usize) -> T {
         self.next_free.push( index );
         let prev = self.data[index].prev;
         let next = self.data[index].next;
@@ -272,18 +382,23 @@ impl<T> Velist<T> {
 
         self.size -= 1;
 
+        if (index == self.first) {
+            self.first = next;
+        }
+
+        self.data[index].content.take().unwrap()
+
        // self.data[index].content;
 
       //  drop( self.data[index].content )
     }
 
-    fn access_by_vague_iterator(&mut self, iterator: AmbigiousIter<T> ) -> &mut T {
-        //if (self. )
-        let index = iterator.index;
-        if ( self.data[index].writes_count != iterator.last_known_id ) {
-            panic!()
-        };
-        return &mut self.data[index].content
+    pub fn get_vague_iter(&self) -> VelistAmbigiousIter<T> {
+        VelistAmbigiousIter {
+            index: self.first,
+            last_known_id: self.data[self.first].writes_count,
+            parent: self as *const Velist<T>
+        }
     }
 
     pub fn get_iter(&mut self) -> VelistIter<T> {
